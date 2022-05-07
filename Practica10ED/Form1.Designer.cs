c4710d3f66920bb54df4ce132ac38524a7fc136d
LucasRZarate <zarate17087@iesmarenostrum.com>
2022-05-07 13:02:05 +0200

    Cambiar Telefono a movil

--- Practica10ED/Form1.Designer.cs
+++ Practica10ED/Form1.Designer.cs
@@ -74,7 +74,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 13);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Telefono:";
+            this.label3.Text = "Movil:";
             // 
             // label4
             // 
